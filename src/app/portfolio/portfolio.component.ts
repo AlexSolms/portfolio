439b7aeb425dec6571f386ede897cae2cfb35231
Alex Solms <duaedin@web.de>
2024-02-05 11:17:36 +0100

    portfolio mobile

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { JoinComponent } from './join/join.component';
+import { ElPolloLocoComponent } from './el-pollo-loco/el-pollo-loco.component';
+import { DaBubbleComponent } from './da-bubble/da-bubble.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [
+    JoinComponent,
+    ElPolloLocoComponent,
+    DaBubbleComponent,
+    PokedexComponent
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
