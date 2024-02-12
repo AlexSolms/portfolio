5baa75aa53df64e3f444eedc4c75c3c2657ed399
Alex Solms <duaedin@web.de>
2024-02-12 11:39:03 +0100

    all commented code of logic change removed

--- src/app/main-content/contact/mail/mail.component.ts
+++ src/app/main-content/contact/mail/mail.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule, NgForm } from '@angular/forms';
-// import { isNullOrUndef } from 'chart.js/dist/helpers/helpers.core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -44,7 +43,6 @@ export class MailComponent {
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
           next: (response) => {
-          //  this.resetStyle();
             ngForm.resetForm();
           },
           error: (error) => {
@@ -53,40 +51,10 @@ export class MailComponent {
           complete: () => console.info('send post complete'),
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-     // this.resetStyle();
       ngForm.resetForm();
     }
   }
 
-
-
- /*  test(filled: boolean, field: string) {
-
-    if (field !== null) {
-      if (filled) {
-        document.getElementById("id" + field)!.classList.add("valid");
-        document.getElementById("idFilled" + field)!.style.display = "block"
-        document.getElementById("idEmpty" + field)!.style.display = "none"; 
-
-      }
-      else {
-        document.getElementById("id" + field)!.classList.remove("valid");
-       document.getElementById("idFilled" + field)!.style.display = "none";
-        document.getElementById("idEmpty" + field)!.style.display = "block"; 
-
-      }
-    }
-
-  }*/
-
- /*  resetStyle(){
-    
-    let inputField = ['Name', 'Email', 'Message'];
-    inputField.forEach(e => {
-      document.getElementById("id" + e)!.classList.remove("valid");
-      document.getElementById("idFilled" + e)!.style.display = "none";
-    });
-  } */
 
 }
 
