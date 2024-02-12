0d5c28165c416bf2cbf82f7e0424511b83b7a83b
Alex Solms <duaedin@web.de>
2024-02-12 11:34:58 +0100

    changed logic in contact to more angular style

--- src/app/main-content/contact/mail/mail.component.ts
+++ src/app/main-content/contact/mail/mail.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule, NgForm } from '@angular/forms';
-import { isNullOrUndef } from 'chart.js/dist/helpers/helpers.core';
+// import { isNullOrUndef } from 'chart.js/dist/helpers/helpers.core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mail',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './mail.component.html',
   styleUrl: './mail.component.scss'
@@ -42,7 +44,7 @@ export class MailComponent {
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
           next: (response) => {
-            this.resetStyle();
+          //  this.resetStyle();
             ngForm.resetForm();
           },
           error: (error) => {
@@ -51,40 +53,40 @@ export class MailComponent {
           complete: () => console.info('send post complete'),
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-      this.resetStyle();
+     // this.resetStyle();
       ngForm.resetForm();
     }
   }
 
 
 
-  test(filled: boolean, field: string) {
+ /*  test(filled: boolean, field: string) {
 
     if (field !== null) {
       if (filled) {
         document.getElementById("id" + field)!.classList.add("valid");
         document.getElementById("idFilled" + field)!.style.display = "block"
-        document.getElementById("idEmpty" + field)!.style.display = "none";
+        document.getElementById("idEmpty" + field)!.style.display = "none"; 
 
       }
       else {
         document.getElementById("id" + field)!.classList.remove("valid");
-        document.getElementById("idFilled" + field)!.style.display = "none";
-        document.getElementById("idEmpty" + field)!.style.display = "block";
+       document.getElementById("idFilled" + field)!.style.display = "none";
+        document.getElementById("idEmpty" + field)!.style.display = "block"; 
 
       }
     }
 
-  }
+  }*/
 
-  resetStyle(){
+ /*  resetStyle(){
     
     let inputField = ['Name', 'Email', 'Message'];
     inputField.forEach(e => {
       document.getElementById("id" + e)!.classList.remove("valid");
       document.getElementById("idFilled" + e)!.style.display = "none";
     });
-  }
+  } */
 
 }
 
